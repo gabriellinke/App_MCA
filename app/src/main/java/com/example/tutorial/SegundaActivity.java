52bd26341e06e08b8d14fa038d9ec3ee7fd2ce53
Gabriel Henrique Linke <gh.linke@hotmail.com>
2020-02-18 22:19:53 -0300

    Botoes de treino programados. Falta fazer a parte referente a cada treino

--- app/src/main/java/com/example/tutorial/SegundaActivity.java
+++ app/src/main/java/com/example/tutorial/SegundaActivity.java
@@ -1,6 +1,9 @@
 package com.example.tutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +15,48 @@ public class SegundaActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_segunda);
+
+        /** BOTÕES QUE REDIRECIONARÃO PARA OS TREINOS ESPECÍFICOS */
+
+        Button botaoTreinoPull = findViewById(R.id.pull_button);
+        Button botaoTreinoPush = findViewById(R.id.push_button);
+        Button botaoTreinoLeg  = findViewById(R.id.leg_button);
+        Button botaoTreinoRun  = findViewById(R.id.run_button);
+
+        botaoTreinoPull.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getBaseContext(), TreinoPull.class));
+            }
+        });
+
+        botaoTreinoPush.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getBaseContext(), TreinoPush.class));
+            }
+        });
+
+        botaoTreinoLeg.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getBaseContext(), TreinoLeg.class));
+            }
+        });
+
+        botaoTreinoRun.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getBaseContext(), TreinoRun.class));
+            }
+        });
     }
 }
