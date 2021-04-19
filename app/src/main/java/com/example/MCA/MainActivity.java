5849ac4bc4895d54423a9ff0c4d3ee6211fb189f
Gabriel Henrique Linke <gh.linke@hotmail.com>
2021-04-19 12:37:06 -0300

    Remoção do serviço de notificações

--- app/src/main/java/com/example/MCA/MainActivity.java
+++ app/src/main/java/com/example/MCA/MainActivity.java
@@ -2,16 +2,10 @@ package com.example.MCA;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.Calendar;
-
 
 public class MainActivity extends AppCompatActivity
 {
@@ -32,27 +26,5 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(getBaseContext(), SegundaActivity.class));
             }
         });
-
-
-        /** USADO PARA CRIAR A NOTIFICAÇÃO DIÁRIA --------------------------------------------------------------------**/
-        Intent broadcastIntent = new Intent(this, AlarmReceiver.class);
-        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        //DEFINE O HORÁRIO DA PRIMEIRA NOTIFICAÇÃO
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 23);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND,0);
-
-        long startUpTime = calendar.getTimeInMillis();
-
-        //REPETE A NOTIFICAÇÃO A CADA 24h
-        if (System.currentTimeMillis() > startUpTime) {
-            startUpTime = startUpTime + 24*60*60*1000;
-        }
-
-        if(alarmManager!=null)
-            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, startUpTime, AlarmManager.INTERVAL_DAY, actionIntent);
     }
 }
